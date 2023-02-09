bff3d063cb51db3dd1d4a79fd253f866b071cf0b
Onyx39 <valentin.richard39@sfr.fr>
2023-02-09 16:07:09 +0100

    update page de connexion

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
-const WebSocket = require('ws');
+//const WebSocket = require('ws');
+
+const MongoClient = require('mongodb').MongoClient;
+
+uri = "mongodb://localhost:42420/mescouilles";
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+async function coucou() {
+  await console.log("salut")
+}
+
+console.log("flag1")
+client.connect().then(async function (){
+  const db = await client.db("mescouilles");
+	const collection = await db.collection("issous");
+  collection.find({});
+  console.log();
+  await coucou();
+  /*
+	let cursor = collec.find({});
+  console.log(cursor)
+	while(await cursor.hasNext()){
+		console.log(await cursor.next());
+  };*/
+
+  const cursor = await collection.findOne({});
+  console.log(cursor)
+});
+
+console.log("Oui")
+/*
 
 const server = new WebSocket.Server({ port: 4420 });
 
@@ -13,3 +44,4 @@ server.on('connection', (socket) => {
     console.log('Client déconnecté');
   });
 });
+*/
