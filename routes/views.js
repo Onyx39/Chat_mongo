45f7b0fe346128d42081eed67a116f1e9782c1d2
Pourquoi? <ryan1991@hotmail.fr>
2023-02-19 03:08:55 +0100

    Chancla

--- routes/views.js
+++ routes/views.js
@@ -7,4 +7,8 @@ viewRouter.get('/', function(req, res){
     res.render('index')
 })
 
+viewRouter.get('/inscription', function(req, res){
+    res.render('inscription')
+})
+
 module.exports = viewRouter
